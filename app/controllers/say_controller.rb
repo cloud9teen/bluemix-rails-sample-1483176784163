3281c461af99d395371b24c9d4013fc6566cca5c
Tim Williams <tim@teachmatic.com>
2015-06-29 09:01:01 +0100

    code tidyup

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -9,26 +9,34 @@ class SayController < ApplicationController
     @rails_version = Rails::VERSION::STRING
     @vcap_services = ENV['VCAP_SERVICES']
     
+    # Execute a request without Static IP
     RestClient.proxy = nil
     json = JSON.parse(RestClient.get("http://ip.jsontest.com"))
     @non_static_result = json["ip"]
-    #@non_static_result = "2.2.2.2"
-    credentials = proxyURL = ''
+
+    proxyURL = extract_proxy_url
+    # Fallback to an environment variable when running locally
+    if proxyURL.blank?
+        proxyURL = ENV["STATICA_URL"]
+    end
+
+    # Execute a request with Static IP
+    RestClient.proxy = proxyURL
+    static_json = JSON.parse(RestClient.get("http://ip.jsontest.com"))
+    @static_result = static_json["ip"]
+  end
+
+
+private
+  def extract_proxy_url
     if !ENV['VCAP_SERVICES'].blank?
       JSON.parse(ENV['VCAP_SERVICES']).each do |k,v|
         if !k.scan("statica").blank?
           credentials = v.first.select {|k1,v1| k1 == "credentials"}["credentials"]
-          proxyURL = credentials["STATICA_URL"]
+          return credentials["STATICA_URL"]
         end
       end
     end
-    if proxyURL.blank?
-        proxyURL = ENV["STATICA_URL"]
-    end
-    RestClient.proxy = proxyURL
-    static_json = JSON.parse(RestClient.get("http://ip.jsontest.com"))
-    @static_result = static_json["ip"]  
-    #@static_result = "1.1.1.1"    
   end
 
 end
